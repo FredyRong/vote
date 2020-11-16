67b55144ffa537629a58549e662dd3bc8d7c0e30
何富荣 <419089660@qq.com>
2020-11-16 15:34:22 +0800

    补充log日志相关配置

--- server/src/main/java/com/fredy/vote/server/controller/HelloController.java
+++ server/src/main/java/com/fredy/vote/server/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.fredy.vote.server;
+package com.fredy.vote.server.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
